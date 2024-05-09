7a738e2a0157870ed888ac4dedccecbdb85480c3
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2024-05-09 14:21:27 +0100

    Adding a responsive screen widget we can reuse everywhere

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hospital_managment_app/styles/palette.dart';
+import 'package:hospital_managment_app/widgets/responsive_screen.dart';
+import 'package:provider/provider.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+
+    final palette = context.watch<Palette>();
+
+    return  Scaffold(
+       backgroundColor: palette.violet,
+      body: ResponsiveScreen(
+        mainAreaProminence: 0.45,
+        squarishMainArea: Container(),
+        rectangularMenuArea: Container(),
+      )
+    );
   }
 }
